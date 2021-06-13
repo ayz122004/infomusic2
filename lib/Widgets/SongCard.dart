67805f37013d1e22d1796154fe070b7681d39fbf
Angelina Zhang <ayz122004@gmail.com>
2021-06-13 16:40:00 -0700

    add favorite icon to SongCard

--- lib/Widgets/SongCard.dart
+++ lib/Widgets/SongCard.dart
@@ -4,23 +4,6 @@ import 'package:infomusic2/GeniusObjects/Song.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 
-Future<Song> fetchSongData(String apiPath) async {
-  final response = await http.get(
-    Uri.parse('https://api.genius.com$apiPath'),
-    headers: {
-      'Accept': 'application/json',
-      'Host': 'api.genius.com',
-      'Authorization':
-          'Bearer pYecHXJg1MiLG_2Hx3FRlZCugQ8Td2FTJi3oOsN-3Zh9eAanCVDPcpPmhefUBWe1',
-    },
-  );
-  if (response.statusCode == 200) {
-    return Song.fromJson(json.decode(response.body));
-  } else {
-    throw Exception('Failed to load song info');
-  }
-}
-
 class SongCard extends StatefulWidget {
   final String apiPath;
   //const SongCard({Key? key}) : super(key: key);
@@ -32,8 +15,24 @@ class SongCard extends StatefulWidget {
 
 class _SongCardState extends State<SongCard> {
   late Future<Song> song;
+  bool isFavorite = false;
 
-
+  Future<Song> fetchSongData(String apiPath) async {
+    final response = await http.get(
+      Uri.parse('https://api.genius.com$apiPath'),
+      headers: {
+        'Accept': 'application/json',
+        'Host': 'api.genius.com',
+        'Authorization':
+            'Bearer pYecHXJg1MiLG_2Hx3FRlZCugQ8Td2FTJi3oOsN-3Zh9eAanCVDPcpPmhefUBWe1',
+      },
+    );
+    if (response.statusCode == 200) {
+      return Song.fromJson(json.decode(response.body));
+    } else {
+      throw Exception('Failed to load song info');
+    }
+  }
 
   @override
   void initState() {
@@ -41,32 +40,37 @@ class _SongCardState extends State<SongCard> {
     song = fetchSongData(widget.apiPath);
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
+  Card buildSongCard() {
+    try {
+      Card card = new Card(
+        margin: EdgeInsets.all(16),
         child: FutureBuilder<Song>(
             future: song,
             builder: (context, snapshot) {
               if (snapshot.hasData) {
-                return Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    ListTile(
-                      leading: new Image.network("${snapshot.data!.imageLink}"),
-                      title: Text("${snapshot.data!.title} by ${snapshot.data!.artist}"),
-                      isThreeLine: true,
-                      subtitle: Text("Album: ${snapshot.data!.album}\nDate: ${snapshot.data!.date}"),
-                    ),
-                  ],
+                return ListTile(
+                  leading: new Image.network("${snapshot.data!.imageLink}"),
+                  trailing: Icon(Icons.favorite_border),
+                  title: Text(
+                      "${snapshot.data!.title} by ${snapshot.data!.artist}"),
+                  isThreeLine: true,
+                  subtitle: Text(
+                      "Album: ${snapshot.data!.album}\nDate: ${snapshot.data!.date}"),
                 );
               } else if (snapshot.hasError) {
                 return Text("${snapshot.error}");
               }
               return Text("loading...");
             }),
-      ),
-    );
+      );
+      return card;
+    } catch (Exception) {
+      return new Card(child: Text("error"));
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return buildSongCard();
   }
 }
